5c2ea351c2514042ceed163079e781a57d294dbe
Emre Akçadağ <akcadagemre@gmail.com>
2019-04-17 20:16:13 +0300

    02-I Am Rich init

---
+++ 02-I Am Rich/02-I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  02-I Am Rich
+//
+//  Created by emre on 17.04.2019.
+//  Copyright © 2019 Emre Akçadağ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
