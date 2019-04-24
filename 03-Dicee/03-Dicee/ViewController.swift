633e99512fab97d020ae765b2e1c0e4739ecb972
Emre Akçadağ <akcadagemre@gmail.com>
2019-04-24 20:02:44 +0300

    03-Dicee init

---
+++ 03-Dicee/03-Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  03-Dicee
+//
+//  Created by emre on 24.04.2019.
+//  Copyright © 2019 Emre Akçadağ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
